ed1c8369af508d27fc18370c392222e9cad11391
Isabella <iisabellappessoa@gmail.com>
2026-03-13 13:43:52 -0300

    Delete Botão directory

--- Botão/script.js
+++
@@ -1,7 +0,0 @@
-let gato = document.getElementById('gatinhogif');
-let botaoJS = document.getElementById('botao');
-
-function ApareceGatinho() {
-    gato.hidden = false;
-    botaoJS.remove(); 
-}
